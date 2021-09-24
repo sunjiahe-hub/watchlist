0ff8823078999f1b6303165d37dc957783503ba9
Sunjiahe <2630017091@qq.com>
2021-09-24 15:10:47 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-from flask import  Flask
+from flask import  Flask, render_template
 from flask import url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/1')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
-@app.route('/bio')
+@app.route('/bio_test')
 def hello_flk():
     # return 'port3 test!'
     return '欢迎来到我的 Watchlist！'
@@ -18,12 +18,33 @@ def user_page(name):
 
 @app.route('/test')
 def test_url_for():
-# 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
     print(url_for('hello')) # 输出：/
-# 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli')) # 输出：/user/greyli
-    print(url_for('user_page', name='peter')) # 输出：/user/peter
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))    # 输出：/user/peter
     print(url_for('test_url_for')) # 输出：/test
-# 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL后面。
-    print(url_for('test_url_for', num=2)) # 输出：/test?num=2
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL后面。
+    print(url_for('test_url_for', num=2))    # 输出：/test?num=2
     return 'Test page'
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
+
+
+name = 'Grey Li'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
